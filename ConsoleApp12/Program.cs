b25428cda2f0c597563d064015060212348bae37
Olajumoke Sekoni <sekoniolajumoke@gmail.com>
2022-03-31 12:51:57 +0100

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lara
+{
+    internal class Ng
+    {
+        static void Main()
+        {
+            Console.WriteLine("Good morning! \n please what is your name?" );
+            string name = Console.ReadLine();
+            Console.WriteLine ( "Hello " + name );
+            Console.WriteLine("Where could you like to go today? \n e.g. Herbert Macuaulay way Lagos");
+            string address = Console.ReadLine();
+            Console.WriteLine("how old are you? ");
+            Console.ReadLine();
+            
+
+
+        }
+       
+    }
+}
